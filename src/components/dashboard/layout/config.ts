b2de5f87f34084bb3d52f2be7b940dc9ec47492d
dannyk-dev <kruger.dkk@gmail.com>
2024-03-25 23:57:37 -0300

    feat(Full Dashboard) implemented dashboard base interface

---
+++ src/components/dashboard/layout/config.ts
@@ -0,0 +1,11 @@
+import type { NavItemConfig } from '@/types/nav';
+import { paths } from '@/paths';
+
+export const navItems = [
+  { key: 'overview', title: 'Overview', href: paths.dashboard.overview, icon: 'chart-pie' },
+  { key: 'maintenace', title: 'Vehicle Maintenance', href: paths.dashboard.maintenance, icon: 'wrench' },
+  { key: 'tracking', title: 'Tracking', href: paths.dashboard.integrations, icon: 'plugs-connected' },
+  { key: 'team', title: 'Team', href: paths.dashboard.team, icon: 'users' },
+  { key: 'settings', title: 'Settings', href: paths.dashboard.settings, icon: 'gear-six' },
+  { key: 'account', title: 'Account', href: paths.dashboard.account, icon: 'user' },
+] satisfies NavItemConfig[];
