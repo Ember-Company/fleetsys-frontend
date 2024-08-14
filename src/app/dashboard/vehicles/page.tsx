8494e641979bb4bfa19d2f17ee5ac4ac51e87648
dannyk-dev <kruger.dkk@gmail.com>
2024-08-14 15:51:47 -0300

    changes

--- src/app/dashboard/vehicles/page.tsx
+++ src/app/dashboard/vehicles/page.tsx
@@ -3,25 +3,29 @@ import type { Metadata } from 'next';
 import { Box } from '@mui/material';
 import Stack from '@mui/material/Stack';
 
-import { VTabsConfig } from '@/types/vehicles';
+import { type VTabsConfig } from '@/types/vehicles';
 import { config } from '@/config';
 import VehicleDataTable from '@/components/dashboard/vehicles/vehicle-list';
 import VehicleTabs from '@/components/dashboard/vehicles/vehicle-tabs';
 
 export const metadata = { title: `Maintenance | Dashboard | ${config.site.name}` } satisfies Metadata;
 
-export default function Page(): React.JSX.Element {
-  const VehicleTabsData: VTabsConfig[] = [
-    {
-      title: 'List',
-      panel: <VehicleDataTable />,
-    },
-    {
-      title: 'Service History',
-      panel: <Box>Ola mundo</Box>,
-    },
-  ];
+const VehicleTabsData: VTabsConfig[] = [
+  {
+    title: 'Overview',
+    panel: <Box>Hello World</Box>,
+  },
+  {
+    title: 'Index',
+    panel: <VehicleDataTable />,
+  },
+  {
+    title: 'Service History',
+    panel: <Box>Ola mundo</Box>,
+  },
+];
 
+export default function Page(): React.JSX.Element {
   return (
     <Stack spacing={3}>
       <VehicleTabs VTabsConfig={VehicleTabsData} />
