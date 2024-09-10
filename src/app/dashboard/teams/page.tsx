88c9ea784f391726601382feb7a1ee911436db03
dannyk-dev <kruger.dkk@gmail.com>
2024-09-10 17:41:35 -0300

    new employee button link added to overview table

--- src/app/dashboard/teams/page.tsx
+++ src/app/dashboard/teams/page.tsx
@@ -1,9 +1,13 @@
 import * as React from 'react';
 import type { Metadata } from 'next';
+import Link from 'next/link';
+import { Button } from '@mui/material';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
+import { Plus } from '@phosphor-icons/react/dist/ssr';
 
 import { config } from '@/config';
+import { paths } from '@/paths';
 import { UsersOverviewTable } from '@/components/dashboard/account/users-overview';
 
 export const metadata = { title: `Teams | Dashboard | ${config.site.name}` } satisfies Metadata;
@@ -15,11 +19,16 @@ export default function TeamsPage(): React.JSX.Element {
         <Stack spacing={1} sx={{ flex: '1 1 auto' }}>
           <Typography variant="h4">Team Members</Typography>
         </Stack>
-        {/* <div>
-          <Button startIcon={<PlusIcon fontSize="var(--icon-fontSize-md)" />} variant="contained">
-            Add
+        <div>
+          <Button
+            startIcon={<Plus fontSize="var(--icon-fontSize-md)" />}
+            variant="contained"
+            LinkComponent={Link}
+            href={paths.dashboard.createEmployee}
+          >
+            New
           </Button>
-        </div> */}
+        </div>
       </Stack>
       <UsersOverviewTable />
     </Stack>
